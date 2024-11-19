21870603ab794e8eb30d37e99e889507dcbb31fd
Zhong <20201783@o365.hanbat.ac.kr>
2024-11-20 00:28:49 +0900

    Player 이동 관련 스크립트 작성

---
+++ Assets/1_Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float _speed;
+    public Vector2 _minBoundary; // 최소 경계
+    public Vector2 _maxBoundary; // 최대 경계
+
+    [Header("Animation Settings")]
+    public Animator _anim;
+
+    [Header("Bullet Settings")]
+    public Transform[] _bullet; // 프리팹 배열
+    public float _delay; // 발사 딜레이
+    public int _playerLevel; // 플레이어 레벨에 따라 변경
+
+    [Header("Transform Settings")]
+    public Transform _myTF; // 플레이어 Transform
+
+    [Header("Sprite Settings")]
+    public SpriteRenderer _spriteRenderer;
+
+    [Header("Internal States")] // 내부적으로 관리하는 상태 변수
+    private float _currentTime; // 시간 체크
+    private bool _isBegine; // 무적 상태 확인
+    private WaitForSeconds _wait = new WaitForSeconds(0.1f); // 반복 대기 시간
+    private Color[] _color; // 무적 상태 시 색상 변화
+    
+    void Awake()
+    {
+        _color = new Color[2];
+        _color[0] = new Color(1, 1, 1, 0.1f);
+        _color[1] = new Color(1, 1, 1, 1);
+    }
+    
+    void Start()
+    {
+        _currentTime = 0;
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 nextPos = new Vector3(h, v, 0) * _speed * Time.deltaTime;
+        Vector3 currentPos = _myTF.position + nextPos;
+        // 이동영역 제한
+        currentPos.x = Mathf.Clamp(currentPos.x, _minBoundary.x, _maxBoundary.x);
+        currentPos.y = Mathf.Clamp(currentPos.y, _minBoundary.y, _maxBoundary.y);
+
+        _myTF.position = currentPos;
+
+        if(Input.GetButtonDown("Vertical") || Input.GetButtonUp("Vertical"))
+        {
+            _anim.SetInteger("Input", (int)v);
+        }
+
+        if(Input.GetButton("Fire1"))
+        {
+            _currentTime += Time.deltaTime;
+            if(_currentTime >= _delay)
+            {
+                // 0으로 하지 말고 깎아주는 식으로 해야 정확한 계산 가능
+                _currentTime -= _delay;
+                Fire();
+            }
+        }
+    }
+
+    void Fire() // 총알 발사 함수
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_isBegine)
+            return;
+        switch (other.tag)
+        {
+            case "enemy":
+                break;
+            case "enemyBullet":
+                break;
+            case "item":
+                break;
+        }
+    }
+}
